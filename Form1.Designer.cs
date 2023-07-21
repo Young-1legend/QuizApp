e3a5b1e7f266ae7985a532b45b245ecde3d3f669
Young-1legend <81308017+Young-1legend@users.noreply.github.com>
2023-07-21 11:18:03 +0000

    initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace QuizApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.lblQuestion = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.questionBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.questionBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(322, 401);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Submit\r\n";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(236, 65);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(103, 20);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "radioButton1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(236, 135);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(103, 20);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "radioButton2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(236, 196);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(103, 20);
+            this.radioButton3.TabIndex = 3;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "radioButton3";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(236, 255);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(103, 20);
+            this.radioButton4.TabIndex = 4;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "radioButton4";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // lblQuestion
+            // 
+            this.lblQuestion.AutoSize = true;
+            this.lblQuestion.Location = new System.Drawing.Point(143, 20);
+            this.lblQuestion.Name = "lblQuestion";
+            this.lblQuestion.Size = new System.Drawing.Size(48, 16);
+            this.lblQuestion.TabIndex = 5;
+            this.lblQuestion.Text = "Label1";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(367, 20);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(357, 308);
+            this.dataGridView1.TabIndex = 6;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // questionBindingSource
+            // 
+            this.questionBindingSource.DataSource = typeof(QuizApp.Form1.Question);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.lblQuestion);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.questionBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.Label lblQuestion;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource questionBindingSource;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace QuizApp
+{
+    public partial class Form1 : Form
+    {
+
+        private List<Question> questions;
+        private int currentQuestionIndex;
+        private int score;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeQuiz();
+        }
+
+        private void InitializeQuiz()
+        {
+            // Initialize the list of questions
+            questions = new List<Question>()
+            
+            {
+                new Question("Which anime is the power system Haki used?", new List<string>{"Demon Slayer", "Dr. Stone", "Jujutsu Kaisen", "One Piece"}, 0),
+                new Question("How many court guards are there in Bleach?", new List<string>{"4", "13, 45", "0"}, 0),
+                new Question("What is the name of Luffy's father?", new List<string>{"Ishigami Senku", "Monkey D. Dragon", "Gojo", "Sasuke"}, 0)
+            };
+            currentQuestionIndex = 0;
+            
+        }
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+
+            if (currentQuestionIndex < questions.Count)
+            {
+                Question currentQuestion = questions[currentQuestionIndex];
+                lblQuestion.Text = currentQuestion.Text;
+                radioButton1.Text = currentQuestion.Options[0];
+                radioButton2.Text = currentQuestion.Options[1];
+                radioButton3.Text = currentQuestion.Options[2];
+                radioButton4.Text = currentQuestion.Options[3];
+
+                // Clear selection
+                radioButton1.Checked = false;
+                radioButton2.Checked = false;
+                radioButton3.Checked = false;
+                radioButton4.Checked = false;
+            }
+            else
+            {
+                // Quiz is over, show the score
+                lblQuestion.Text = "Quiz Over!";
+                MessageBox.Show($"Your Score: {score}/{questions.Count}");
+
+
+            }
+        }
+
+
+            private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            {
+                if (currentQuestionIndex < questions.Count)
+                {
+                    if (radioButton1.Checked || radioButton2.Checked || radioButton3.Checked || radioButton4.Checked)
+                    {
+                        int selectedOptionIndex = GetSelectedOptionIndex();
+
+                        if (selectedOptionIndex == questions[currentQuestionIndex].CorrectOptionIndex)
+                        {
+                            score++;
+                        }
+
+                        currentQuestionIndex++;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Please select an option!");
+                    }
+                }
+            }
+
+             int GetSelectedOptionIndex()
+            {
+                if (radioButton1.Checked)
+                    return 0;
+                else if (radioButton2.Checked)
+                    return 1;
+                else if (radioButton3.Checked)
+                    return 2;
+                else if (radioButton4.Checked)
+                    return 3;
+                else
+                    return -1;
+            }
+        }
+
+        public class Question
+        {
+            public string Text { get; set; }
+            public List<string> Options { get; set; }
+            public int CorrectOptionIndex { get; set; }
+
+            public Question(string text, List<string> options, int correctOptionIndex)
+            {
+                Text = text;
+                Options = options;
+                CorrectOptionIndex = correctOptionIndex;
+            }
+        }
+
+       
+            }
+        }
+    
+
+
